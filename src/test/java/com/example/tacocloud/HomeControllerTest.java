a8adab892e58be6f33e3496014db097c059f08e7
Gaurav Singh Rana <gauravrana.dce@gmail.com>
2020-09-30 23:23:51 +0530

    first commit

---
+++ src/test/java/com/example/tacocloud/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.tacocloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.containsString;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("home")).
+                andExpect(content().string(containsString("Welcome to...")));
+    }
+
+}
