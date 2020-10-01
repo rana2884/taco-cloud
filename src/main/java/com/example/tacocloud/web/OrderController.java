46043734ddd7930dba1f3200b281a97a9f74781b
Gaurav Singh Rana <gauravrana.dce@gmail.com>
2020-10-02 00:42:24 +0530

    second

---
+++ src/main/java/com/example/tacocloud/web/OrderController.java
@@ -0,0 +1,30 @@
+package com.example.tacocloud.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+import com.example.tacocloud.Order;
+
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model){
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order, Model model){
+        log.info("Order submitted: "+ order);
+        return "redirect:/";
+    }
+}
